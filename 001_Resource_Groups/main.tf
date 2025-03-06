9009ae3443b8e71272571d7c321659d9f1581e42
Jack Roper <39914569+jackwesleyroper@users.noreply.github.com>
2025-03-06 11:50:07 +0000

    updating version constraints

--- 001_Resource_Groups/main.tf
+++ 001_Resource_Groups/main.tf
@@ -1,14 +1,14 @@
 terraform {
-  required_version = "1.10.5"
+  required_version = ">=1.10.5"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.16.0"
+      version = ">=4.16.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "2.53.1"
+      version = ">=2.53.1"
     }
   }
 
--- 007_Containers/main.tf
+++ 007_Containers/main.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = "1.10.5"
+  required_version = ">=1.10.5"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.16.0"
+      version = ">=4.16.0"
     }
     tls = {
       source  = "hashicorp/tls"
@@ -12,7 +12,7 @@ terraform {
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "2.53.1"
+      version = ">=2.53.1"
     }
   }
 
